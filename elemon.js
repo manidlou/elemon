93a6614e3cce15c07dbfb028d17c18b00e9b645f
Mawni Maghsoudlou <maawni@gmail.com>
2016-05-18 07:35:16 -0700

    wrap the proc in a function

--- elemon.js
+++ elemon.js
@@ -2,26 +2,9 @@
 
 /*!
  * elemon.js
- * This software is released under the MIT license:
  * 
  * Copyright (c) 2016 Mawni Maghsoudlou
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy of
- * this software and associated documentation files (the "Software"), to deal in
- * the Software without restriction, including without limitation the rights to
- * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
- * the Software, and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
- * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
- * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
- * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * Released under the MIT license
  */
 
 /**
@@ -29,43 +12,44 @@
  * automatically reloads the application upon any changes.
  */
 
-'use strict';
+(function() {
+  'use strict';
 
-const watch = require('node-watch');
-const spawn = require('child_process').spawn;
-const ELEC_BIN = './node_modules/.bin/electron';
+  const watch = require('node-watch');
+  const spawn = require('child_process').spawn;
+  const ELEC_BIN = './node_modules/.bin/electron';
 
-var elec_proc = spawn(ELEC_BIN, ['.'], {detached: true});
+  var elec_proc = spawn(ELEC_BIN, ['.'], {detached: true});
 
-elec_proc.on('error', function(err) {
-  console.log('|\n-> elemon error: failed to start electron main process -> ', err);
-  elec_proc.stdin.end();
-  process.kill(-elec_proc.pid);
-  process.exit(0);
-});
+  elec_proc.on('error', function(err) {
+    console.log('|\n-> elemon error: failed to start electron main process -> ', err);
+    elec_proc.stdin.end();
+    process.kill(-elec_proc.pid);
+    process.exit(0);
+  });
 
-process.on('SIGINT', function() {
-  elec_proc.stdin.end();
-  process.kill(-elec_proc.pid);
-  process.exit(0);
-});
+  process.on('SIGINT', function() {
+    elec_proc.stdin.end();
+    process.kill(-elec_proc.pid);
+    process.exit(0);
+  });
 
-process.on('SIGTERM', function() {
-  elec_proc.stdin.end();
-  process.kill(-elec_proc.pid);
-  process.exit(0);
-});
+  process.on('SIGTERM', function() {
+    elec_proc.stdin.end();
+    process.kill(-elec_proc.pid);
+    process.exit(0);
+  });
 
-elec_proc.stderr.on('data', function(data) {
-  console.log('|\n-> elemon error: ', data);
-  elec_proc.stdin.end();
-  process.kill(-elec_proc.pid);
-  process.exit(0);
-});
+  elec_proc.stderr.on('data', function(data) {
+    console.log('|\n-> elemon error: ', data);
+    elec_proc.stdin.end();
+    process.kill(-elec_proc.pid);
+    process.exit(0);
+  });
 
-watch(process.cwd(), function() {
-  elec_proc.stdin.end();
-  process.kill(-elec_proc.pid);
-  elec_proc = spawn(ELEC_BIN, ['.'], {detached: true});
-});
-
+  watch(process.cwd(), function() {
+    elec_proc.stdin.end();
+    process.kill(-elec_proc.pid);
+    elec_proc = spawn(ELEC_BIN, ['.'], {detached: true});
+  });
+}());
